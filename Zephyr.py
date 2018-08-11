9c0fcbf6c4f5a9aae0244190353de0a71475493f
Quinn Ebert <QuinnEbert@users.noreply.github.com>
2018-08-11 15:01:14 -0400

    Rebase Zephy from sprite_move_walls.py

--- Zephyr.py
+++ Zephyr.py
@@ -1,163 +1,122 @@
 import arcade
-import sys
-import pygame
+import os
 
-SCREEN_WIDTH = 640
-SCREEN_HEIGHT = 480
+SPRITE_SCALING = 2.0
 
-KEY_Q = 113
-KEY_W = 119
-KEY_S = 115
-KEY_A = 97
-KEY_D = 100
-KEY_R = 114
-KEYS_HELD = []
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+MOVEMENT_SPEED = 5
+
 
 class MyGame(arcade.Window):
-    tiger_posx = SCREEN_WIDTH / 2
-    tiger_posy = SCREEN_HEIGHT / 2
-    score = 0
-    tiger = None
-    viewport_offset_x = 0
-    viewport_offset_y = 0
-    walls = []
-    walls_pe = SpriteList()
-    w = []
+    """ Main application class. """
 
     def __init__(self, width, height):
+        """
+        Initializer
+        """
         super().__init__(width, height)
 
-        # Build the walls
-        self.w = [
-            [1,1,1,1,1,1,1,1,1,1,1,1],
-            [1,0,0,0,0,0,0,0,0,0,0,0],
-            [1,0,0,0,0,0,0,0,0,0,0,1],
-            [1,0,0,0,0,0,0,0,0,0,0,1],
-            [1,0,0,0,0,0,0,0,0,0,0,1],
-            [1,0,0,0,0,0,0,0,0,0,0,1],
-            [1,0,0,0,0,0,0,0,0,0,0,1],
-            [1,1,1,1,1,1,1,1,1,1,1,1]
-        ]
-        x = 0
-        while x < len(self.w):
-            y = 0
-            self.walls.append([])
-            while y < len(self.w[x]):
-                if self.w[x][y]:
-                    new_wall = arcade.Sprite("brick.jpg", 1.0)
-                    self.walls[x].append(new_wall)
-                    walls_pe.append(new_wall)
-                else:
-                    self.walls[x].append(None)
-                y += 1
-            x += 1
+        # Set the working directory (where we expect to find files) to the same
+        # directory this .py file is in. You can leave this out of your own
+        # code, but it is needed to easily run the examples using "python -m"
+        # as mentioned at the top of this program.
+        file_path = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(file_path)
 
-        pygame.mixer.init()
-        pygame.mixer.music.load("C:\\Windows\\Media\\tada.wav")
-        arcade.set_background_color(arcade.color.BLACK)
-        self.reload()
+        # Sprite lists
+        self.all_sprites_list = None
+        self.coin_list = None
 
-    def reload(self):
-        self.tiger_posx = SCREEN_WIDTH / 2
-        self.tiger_posy = SCREEN_HEIGHT / 2
+        # Set up the player
         self.score = 0
-
-    def on_key_press(self, key, modifiers):
-        if key == KEY_Q:
-            sys.exit(0)
-        if key == KEY_R and self.score:
-            self.reload()
-        else:
-            if not key in KEYS_HELD:
-                KEYS_HELD.append(key)
-    def on_key_release(self, key, modifiers):
-        if key in KEYS_HELD:
-            KEYS_HELD.remove(key)
+        self.player_sprite = None
+        self.wall_list = None
+        self.physics_engine = None
 
     def setup(self):
-        # Set up your game here
-        pass
+        """ Set up the game and initialize the variables. """
+
+        # Sprite lists
+        self.all_sprites_list = arcade.SpriteList()
+        self.wall_list = arcade.SpriteList()
+
+        # Set up the player
+        self.score = 0
+        self.player_sprite = arcade.Sprite("tiger.png",
+                                           SPRITE_SCALING)
+        self.player_sprite.center_x = 50
+        self.player_sprite.center_y = 64
+        self.all_sprites_list.append(self.player_sprite)
+
+        # -- Set up the walls
+        # Create a row of boxes
+        for x in range(173, 650, 64):
+            wall = arcade.Sprite("brick.jpg", SPRITE_SCALING)
+            wall.center_x = x
+            wall.center_y = 200
+            self.all_sprites_list.append(wall)
+            self.wall_list.append(wall)
+
+        # Create a column of boxes
+        for y in range(273, 500, 64):
+            wall = arcade.Sprite("brick.jpg", SPRITE_SCALING)
+            wall.center_x = 465
+            wall.center_y = y
+            self.all_sprites_list.append(wall)
+            self.wall_list.append(wall)
+
+        self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite,
+                                                         self.wall_list)
+
+        # Set the background color
+        arcade.set_background_color(arcade.color.AMAZON)
 
     def on_draw(self):
-        """ Render the screen. """
-        arcade.start_render()
-        SPRITE_SCALING_TIGER = 0.5
-        self.tiger = arcade.Sprite("tiger.png", SPRITE_SCALING_TIGER)
-        self.tiger.set_position(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-        self.tiger.draw()
-        x = 0
-        while x < len(self.walls):
-            y = 0
-            while y < len(self.walls[x]):
-                try:
-                    self.walls[x][y].set_position(((y)*32+self.viewport_offset_x), ((-x)*32+self.viewport_offset_y))
-                    self.walls[x][y].draw()
-                except AttributeError:
-                    pass
-                y += 1
-            x += 1
-        output = f"View: {self.viewport_offset_x}, {self.viewport_offset_y}"
-        arcade.draw_text(output, 10, 40, arcade.color.WHITE, 14)
-        output = f"Score: {self.score}"
-        arcade.draw_text(output, 10, 25, arcade.color.WHITE, 14)
-        if self.score == 3:
-            output = f"You win!  Press Q to exit or R to replay"
-            arcade.draw_text(output, 10, 10, arcade.color.WHITE, 14)
+        """
+        Render the screen.
+        """
 
+        # This command has to happen before we start drawing
+        arcade.start_render()
+
+        # Draw all the sprites.
+        self.wall_list.draw()
+        self.player_sprite.draw()
+
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed. """
+
+        if key == arcade.key.UP:
+            self.player_sprite.change_y = MOVEMENT_SPEED
+        elif key == arcade.key.DOWN:
+            self.player_sprite.change_y = -MOVEMENT_SPEED
+        elif key == arcade.key.LEFT:
+            self.player_sprite.change_x = -MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT:
+            self.player_sprite.change_x = MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key. """
+
+        if key == arcade.key.UP or key == arcade.key.DOWN:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
+            self.player_sprite.change_x = 0
 
     def update(self, delta_time):
-        """ All the logic to move, and the game logic goes here. """
-        for key in KEYS_HELD:
-            if key == KEY_W:
-                #self.tiger_posy += 4
-                self.viewport_offset_y -= 2
-            if key == KEY_S:
-                #self.tiger_posy -= 4
-                self.viewport_offset_y += 2
-            if key == KEY_A:
-                #self.tiger_posx -= 4
-                self.viewport_offset_x += 2
-            if key == KEY_D:
-                #self.tiger_posx += 4
-                self.viewport_offset_x -= 2
-        try:
-            self.tiger.set_position(self.tiger_posx, self.tiger_posy)
-            self.tiger.draw()
-            x = 0
-            while x < len(self.walls):
-                y = 0
-                while y < len(self.walls[x]):
-                    try:
-                        colres = arcade.geometry.check_for_collision(self.tiger, self.walls[x][y])
-                        # Rudimentary backoff
-                        if colres:
-                            print("BUMP")
-                            for key in KEYS_HELD:
-                                if key == KEY_W:
-                                    #self.tiger_posy += 4
-                                    self.viewport_offset_y += 3
-                                if key == KEY_S:
-                                    #self.tiger_posy -= 4
-                                    self.viewport_offset_y -= 3
-                                if key == KEY_A:
-                                    #self.tiger_posx -= 4
-                                    self.viewport_offset_x -= 3
-                                if key == KEY_D:
-                                    #self.tiger_posx += 4
-                                    self.viewport_offset_x += 3
-                    except TypeError:
-                        pass
-                    y += 1
-                x += 1
-        except AttributeError:
-            # This is a cheap hack for a failure that only happens on the first
-            # frame...
-            pass
+        """ Movement and game logic """
+
+        # Call update on all sprites (The sprites don't do much in this
+        # example though.)
+        self.physics_engine.update()
 
 
 def main():
-    game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT)
-    game.setup()
+    """ Main method """
+    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT)
+    window.setup()
     arcade.run()
 
 
